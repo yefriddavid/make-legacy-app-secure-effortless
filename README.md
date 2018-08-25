5eef797fefa3198b60d949882bd126e4fec7fa52
Jeffrey Rios <yefriddavid@gmail.com>
2018-08-25 09:08:05 -0300

    Polis with user and password

---
+++ README.md
@@ -0,0 +1,9 @@
+# Docs
+
+
+cd node
+npm install
+node app.js && 
+cd ..
+cd nginx
+docker-compose up -d
--- node/app.js
+++ node/app.js
@@ -1,9 +1,19 @@
 const express = require('express')
+var auth = require('basic-auth')
+
 const app = express()
 
 app.get('/', (req, res, next) => {
-	console.log("ssss")
-	next()
+  var credentials = auth(req)
+
+  console.log("ssss")
+  if (!credentials || credentials.name !== 'john' || credentials.pass !== 'secret') {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic realm="example"')
+    res.end('Access denied')
+  } else {
+    next()
+  }
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
